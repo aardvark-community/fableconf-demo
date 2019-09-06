202ea09e89a0d232a4feefb9e83fb1c18778dc95
Harald Steinlechner <haraldsteinlechner@gmail.com>
2019-09-06 08:38:45 +0200

    sync

--- src/ElmSpheres/Program.fs
+++ src/ElmSpheres/Program.fs
@@ -9,6 +9,54 @@ open Aardvark.Rendering.Vulkan
 open Aardvark.Base
 open System
 
+open Aardvark.Base.Incremental
+type Changes = unit
+
+
+
+type Object = {  name : string; pos : V3d;  }
+
+type Scene = 
+    {
+        cameraPos : V3d
+        objects : list<Object>
+    }
+
+type MScene =
+    {
+        mcameraPos : IModRef<V3d>
+        mobjects   : list<Object>
+    }
+
+
+let oldScene : Scene  =
+    {
+        cameraPos = V3d(10,10,10)
+        objects = [ { name = "Chair"; pos = V3d(0,0,0) }]
+    }
+
+let newScene : Scene  =
+    {
+        cameraPos = V3d(10,10,10)
+        objects = [  { name = "Chair"; pos = V3d(0,10,0)}
+                     { name = "harry"; pos = V3d(10,10,10) } 
+                  ]
+    }
+
+let computeChanges (old : Scene) (newScene : Scene) : Changes = 
+    failwith "todo smart implementation"
+
+
+
+let applyChanges (old : Scene) (newScene : Scene) (state : MScene) : unit = 
+    transact (fun _ -> 
+        if old.cameraPos <> newScene.cameraPos then
+            state.mcameraPos.Value <- newScene.cameraPos
+        // same, for set but more complicated..
+    )
+
+// old = new -> nothing to do
+// finegrained changes fed into incremental system
 
 
 
